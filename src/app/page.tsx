fad84c6f06df4786d7fa25cc2f12fd15339206f9
Sahil Verma <domainsahil007@gmail.com>
2024-03-04 22:19:02 +0530

    Unlike any tool section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { classNames } from "@/utils";
 import { ClientsSection } from "./_components/sections/clients";
 import { HeroSection } from "./_components/sections/hero-section";
 import Container from "./_components/shared/container";
+import { StarsIllustration } from "./_components/icons/stars";
+import { UnlikeAnyToolSection } from "./_components/sections/unlike-any-tool";
 
 const Home = () => {
   return (
@@ -12,6 +15,18 @@ const Home = () => {
       </div>
       <Container>
         <ClientsSection />
+      </Container>
+      <div
+        className={classNames(
+          "mask-radial-faded pointer-events-none relative z-[-1] my-[-12.8rem] h-[60rem] overflow-hidden",
+          "[--color:#7877C6] before:absolute before:inset-0 before:bg-radial-faded before:opacity-[0.4]",
+          "after:absolute after:top-1/2 after:-left-1/2 after:h-[142.8%] after:w-[200%] after:rounded-[50%] after:border-t after:border-[rgba(120,_119,_198,_0.4)] after:bg-background"
+        )}
+      >
+        <StarsIllustration />
+      </div>
+      <Container>
+        <UnlikeAnyToolSection />
       </Container>
     </>
   );
